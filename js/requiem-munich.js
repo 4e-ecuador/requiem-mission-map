50bd3c27c0bac2dd77d648087fee85d1de371aae
Nikolai Plath <der.el.kuku@gmail.com>
2020-03-06 10:07:45 -0500

    Start work on country chart

--- js/requiem-munich.js
+++ js/requiem-munich.js
@@ -10,28 +10,64 @@ const markers = L.markerClusterGroup({disableClusteringAtZoom: 14});
 map.addLayer(markers);
 
 let replacements = ['city', 'country', 'imid', 'creator'];
-let htmlTemplate = '{{city}}, {{country}}<br />' + 
-                   '<a target="_blank" href="https://ingressmosaic.com/mosaic/{{imid}}">IM</a>' + 
-                   ' by {{creator}}';
+let htmlTemplate = '{{city}}, {{country}}<br />' +
+    '<a target="_blank" href="https://ingressmosaic.com/mosaic/{{imid}}">IM</a>' +
+    ' by {{creator}}';
+let chartData = {};
+
 function getHtml(obj) {
-    var html = htmlTemplate;
-    $(replacements).each(function(i, replacement){
-        html = html.replace('{{'+replacement+'}}', obj[replacement]);
+    let html = htmlTemplate;
+    $(replacements).each(function (i, replacement) {
+        html = html.replace('{{' + replacement + '}}', obj[replacement]);
     });
     return html;
 }
 
 $.get('missions.json', {some_var: ''}, function (data) {
+    let chartInfo = [];
     $('#mission-counter').html(data.length);
     $(data).each(function () {
         let marker = new L.Marker(new L.LatLng(this.lat, this.lng), {title: this.city});
         marker.bindPopup(getHtml(this));
         markers.addLayer(marker);
+        if (chartInfo[this.country] === undefined) {
+            chartInfo[this.country] = 1;
+        } else {
+            chartInfo[this.country]++;
+        }
     });
+
+    chartData['labels'] = []
+    chartData['data'] = []
+    for (const key in chartInfo) {
+        if (chartInfo.hasOwnProperty(key)) {
+            chartData['labels'].push(key)
+            chartData['data'].push(chartInfo[key]);
+        }
+    }
 }, 'json');
+
+$('#btn-chart').on("click", function () {
+    const ctx = document.getElementById('myChart').getContext('2d');
+    let data = {
+        datasets: [{
+            data: chartData['data']
+        }],
+        labels: chartData['labels']
+    };
+    let options = {}
+    const myPieChart = new Chart(ctx, {
+        type: 'pie',
+        data: data,
+        options: options
+    });
+
+    $('#chartModal').modal()
+})
 
 // Logo
 const mapControlsContainer = document.getElementsByClassName("leaflet-control")[0];
 
 mapControlsContainer.appendChild(document.getElementById("logoContainer"));
 mapControlsContainer.appendChild(document.getElementById("mission-counter"));
+mapControlsContainer.appendChild(document.getElementById("controls-container"));
