5c2aeab12266bf6eaaa9e29d1f3a42ec8679763e
sebastian groemcke <sg@dplusc.de>
2020-03-06 19:42:14 +0100

    Added Rennes 2/2

--- js/requiem-munich.js
+++ js/requiem-munich.js
@@ -16,7 +16,7 @@ let htmlTemplate = '{{city}}, {{country}}<br />' +
 function getHtml(obj) {
     var html = htmlTemplate;
     $(replacements).each(function(i, replacement){
-        html = html.replace('{{'+replacement+'}}', obj[replacement]);
+        html = html.replace('{{'+replacement+'}}', obj[replacement].trim());
     });
     return html;
 }
